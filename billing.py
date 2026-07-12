73b8a21cbfeb5f6f3c86d680d368761612559a93
santhosh-murugesan <sandy0leymar@gmail.com>
2026-07-12 23:18:56 +0530

    code has been modified

--- billing.py
+++ billing.py
@@ -1,1 +1,11 @@
 #this is space to write the billing code for the github demo
+
+def process_payment(amount, card_number, expiration_date, cvv):
+    # Placeholder for payment processing logic
+    if amount > 0 and len(card_number) == 16 and len(cvv) == 3:
+        return True
+    else:
+        return False
+    
+
+    
--- database.py
+++ database.py
@@ -1,1 +1,5 @@
 #this is sapce to database code for the github demo
+
+def connect_to_database():
+    # Placeholder for database connection logic
+    return True
